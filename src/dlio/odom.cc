563289b0c1ced12bbbf5018a90287d34ee431f6b
Fang <anannxf@gmail.com>
2025-06-08 23:44:11 +0200

    integration

--- src/dlio/odom.cc
+++ src/dlio/odom.cc
@@ -382,58 +382,59 @@ void dlio::OdomNode::publishToROS(pcl::PointCloud<PointType>::ConstPtr published
   this->path_ros.poses.push_back(p);
   this->path_pub.publish(this->path_ros);
 
-  // transform: odom to baselink
-  static tf2_ros::TransformBroadcaster br;
-  geometry_msgs::TransformStamped transformStamped;
+  // // transform: odom to baselink
+  // static tf2_ros::TransformBroadcaster br;
+  // geometry_msgs::TransformStamped transformStamped;
 
-  transformStamped.header.stamp = this->imu_stamp;
-  transformStamped.header.frame_id = this->odom_frame;
-  transformStamped.child_frame_id = this->baselink_frame;
+  // transformStamped.header.stamp = this->imu_stamp;
+  // transformStamped.header.frame_id = this->odom_frame;
+  // transformStamped.child_frame_id = this->baselink_frame;
 
-  transformStamped.transform.translation.x = this->state.p[0];
-  transformStamped.transform.translation.y = this->state.p[1];
-  transformStamped.transform.translation.z = this->state.p[2];
+  // transformStamped.transform.translation.x = this->state.p[0];
+  // transformStamped.transform.translation.y = this->state.p[1];
+  // transformStamped.transform.translation.z = this->state.p[2];
 
-  transformStamped.transform.rotation.w = this->state.q.w();
-  transformStamped.transform.rotation.x = this->state.q.x();
-  transformStamped.transform.rotation.y = this->state.q.y();
-  transformStamped.transform.rotation.z = this->state.q.z();
+  // transformStamped.transform.rotation.w = this->state.q.w();
+  // transformStamped.transform.rotation.x = this->state.q.x();
+  // transformStamped.transform.rotation.y = this->state.q.y();
+  // transformStamped.transform.rotation.z = this->state.q.z();
 
-  br.sendTransform(transformStamped);
+  // br.sendTransform(transformStamped);
 
-  // transform: baselink to imu
-  transformStamped.header.stamp = this->imu_stamp;
-  transformStamped.header.frame_id = this->baselink_frame;
-  transformStamped.child_frame_id = this->imu_frame;
+  // Do not publish these static transforms
+  // // transform: baselink to imu
+  // transformStamped.header.stamp = this->imu_stamp;
+  // transformStamped.header.frame_id = this->baselink_frame;
+  // transformStamped.child_frame_id = this->imu_frame;
 
-  transformStamped.transform.translation.x = this->extrinsics.baselink2imu.t[0];
-  transformStamped.transform.translation.y = this->extrinsics.baselink2imu.t[1];
-  transformStamped.transform.translation.z = this->extrinsics.baselink2imu.t[2];
+  // transformStamped.transform.translation.x = this->extrinsics.baselink2imu.t[0];
+  // transformStamped.transform.translation.y = this->extrinsics.baselink2imu.t[1];
+  // transformStamped.transform.translation.z = this->extrinsics.baselink2imu.t[2];
 
-  Eigen::Quaternionf q(this->extrinsics.baselink2imu.R);
-  transformStamped.transform.rotation.w = q.w();
-  transformStamped.transform.rotation.x = q.x();
-  transformStamped.transform.rotation.y = q.y();
-  transformStamped.transform.rotation.z = q.z();
+  // Eigen::Quaternionf q(this->extrinsics.baselink2imu.R);
+  // transformStamped.transform.rotation.w = q.w();
+  // transformStamped.transform.rotation.x = q.x();
+  // transformStamped.transform.rotation.y = q.y();
+  // transformStamped.transform.rotation.z = q.z();
 
-  br.sendTransform(transformStamped);
+  // br.sendTransform(transformStamped);
 
-  // transform: baselink to lidar
-  transformStamped.header.stamp = this->imu_stamp;
-  transformStamped.header.frame_id = this->baselink_frame;
-  transformStamped.child_frame_id = this->lidar_frame;
+  // // transform: baselink to lidar
+  // transformStamped.header.stamp = this->imu_stamp;
+  // transformStamped.header.frame_id = this->baselink_frame;
+  // transformStamped.child_frame_id = this->lidar_frame;
 
-  transformStamped.transform.translation.x = this->extrinsics.baselink2lidar.t[0];
-  transformStamped.transform.translation.y = this->extrinsics.baselink2lidar.t[1];
-  transformStamped.transform.translation.z = this->extrinsics.baselink2lidar.t[2];
+  // transformStamped.transform.translation.x = this->extrinsics.baselink2lidar.t[0];
+  // transformStamped.transform.translation.y = this->extrinsics.baselink2lidar.t[1];
+  // transformStamped.transform.translation.z = this->extrinsics.baselink2lidar.t[2];
 
-  Eigen::Quaternionf qq(this->extrinsics.baselink2lidar.R);
-  transformStamped.transform.rotation.w = qq.w();
-  transformStamped.transform.rotation.x = qq.x();
-  transformStamped.transform.rotation.y = qq.y();
-  transformStamped.transform.rotation.z = qq.z();
+  // Eigen::Quaternionf qq(this->extrinsics.baselink2lidar.R);
+  // transformStamped.transform.rotation.w = qq.w();
+  // transformStamped.transform.rotation.x = qq.x();
+  // transformStamped.transform.rotation.y = qq.y();
+  // transformStamped.transform.rotation.z = qq.z();
 
-  br.sendTransform(transformStamped);
+  // br.sendTransform(transformStamped);
 
 }
 
